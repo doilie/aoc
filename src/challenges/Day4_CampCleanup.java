1e5535a89df955337dfe6aa3aa06b4ae656e2cdd
doilie <metz.laurito@gmail.com>
2023-09-27 16:46:00 +0800

    Day 4

---
+++ src/challenges/Day4_CampCleanup.java
@@ -0,0 +1,62 @@
+package challenges;
+
+import impl.cleanup.ElfPairAssignment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day4_CampCleanup extends Challenge {
+    public static void main(String[] args)  {
+        Day4_CampCleanup day4 = new Day4_CampCleanup();
+        day4.doOneStarSolution();
+        day4.doTwoStarSolution();
+    }
+
+    public Day4_CampCleanup() {
+        super("day4-input.txt");
+        parseFile();
+    }
+
+    private final List<ElfPairAssignment> elfPairAssignments = new ArrayList<>();
+
+    @Override
+    protected void parseFile() {
+        String[] lines = this.getFileContents().split("\n");
+        for (String line : lines) {
+            if (line != null) {
+                String[] pairs = line.split(",");
+                if (pairs.length == 2) {
+                    String[] elf1 = pairs[0].split("-");
+                    String[] elf2 = pairs[1].split("-");
+                    if (elf1.length == 2 && elf2.length == 2) {
+                        elfPairAssignments.add(
+                                new ElfPairAssignment(Integer.parseInt(elf1[0]), Integer.parseInt(elf1[1]),
+                                                        Integer.parseInt(elf2[0]), Integer.parseInt(elf2[1])));
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void doOneStarSolution() {
+        int fullyOverlapCount = 0;
+        for (ElfPairAssignment elfPair : elfPairAssignments) {
+            if (elfPair.doesAssignmentFullyOverlap()) {
+                fullyOverlapCount++;
+            }
+        }
+        System.out.println("Fully overlap count: " + fullyOverlapCount);
+    }
+
+    @Override
+    public void doTwoStarSolution() {
+        int partialOverlapCount = 0;
+        for (ElfPairAssignment elfPair : elfPairAssignments) {
+            if (elfPair.doesAssignmentOverlap()) {
+                partialOverlapCount++;
+            }
+        }
+        System.out.println("Partial overlap count: " + partialOverlapCount);
+    }
+}
